ccf26890ae768200255cd75d7a37660e1df90846
Alex <alex.romero.605@my.csun.edu>
2019-05-01 00:18:29 -0700

    updated some ios files to get it running on ios again (still have google signon issues)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,11 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override init() {
+    FirebaseApp.configure()
+    }
 }
